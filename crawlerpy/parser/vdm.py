db9024c4d3b9e98ce91949cad0edbb9e9ec92a85
Johann Lecocq <git-jo@johann-lecocq.fr>
2017-01-21 14:42:17 +0100

    update parser vdm

--- crawlerpy/parser/vdm.py
+++ crawlerpy/parser/vdm.py
@@ -10,28 +10,34 @@ from crawlerpy.parser import Parser,ParseException
 from crawlerpy.objet import *
 
 class VdmParser(Parser):
-	"""The implementation of VideDeMerde site parser"""
-	def __init__(self):
-		pass
-	def parse(self,text):
-		try:
-			reponse=[]
-			soup = BeautifulSoup(text, 'html.parser')
-			articles=soup.find_all("div")
-			for article in articles:
-				attribut=article.get("class")
-				if attribut is None or "article" not in attribut:
-					continue
-				article=article.find("a")
-				attribut=article.get("class")
-				if  attribut is None or "fmllink" not in attribut:
-					continue
-				section=Section("text")
-				section.add_content(Data("string",article.getText()))
-				article=Article(article.get("href")[1::].replace("/","_"))
-				article.add_section(section)
-				reponse.append(article)
-			return reponse
-		except Exception as e:
-			print(e)
-			raise ParseException()
+    """The implementation of VideDeMerde site parser"""
+    def __init__(self):
+        pass
+    def parse(self,text):
+        print("je passe dans le parser")
+        try:
+            reponse=[]
+            soup = BeautifulSoup(text, 'html.parser')
+            articles=soup.find_all("article")
+            print(len(articles))
+            for article in articles:
+                attribut=article.get("class")
+                if attribut is None or "art-panel" not in attribut:
+                    continue
+                block=article.find("p")
+                content=block.find("a")
+                print(content.get("href"))
+                print(content.getText())
+                if 'http://www.public.fr' in content.get("href") or "http://people.premiere.fr" in content.get("href"):
+                    continue
+                section=Section("text")
+                section.add_content(Data("string",content.getText()))
+                l=content.get("href").replace("/article/","").replace("/","__").replace(".html","")
+                a=Article(l)
+                a.add_section(section)
+                reponse.append(a)
+            print(reponse,len(reponse))
+            return reponse
+        except Exception as e:
+            print(e)
+            raise ParseException()
--- crawlerpy/vdm.py
+++ crawlerpy/vdm.py
@@ -13,41 +13,45 @@ from crawlerpy.parser.vdm import VdmParser
 LIEN_ACCUEIL = "http://www.viedemerde.fr"
 LIEN_ALEATOIRE = "http://www.viedemerde.fr/aleatoire"
 LIEN_PAGE = "http://www.viedemerde.fr/?page=%s"
-LIEN_ARTICLE = "http://www.viedemerde.fr/%s"
+LIEN_ARTICLE = "http://www.viedemerde.fr/article/%s"
 
 class VdmCrawler(ArticleCrawler):
-	"""The implementation of VieDeMerde crawler"""
-	def __init__(self):
-		super().__init__(VdmParser(), HttpCommunicator())
-	def __go(self, lien):
-		reponse = self.aspirateur.get(lien)
-		if reponse["code"] == 200:
-			text = unescape(reponse["data"])
-			try:
-				data = self.parser.parse(text)
-			except ParseException:
-				return(521,[])
-			code = 200
-		else:
-			code = reponse["code"]
-			data = []
-		return (code, data)
-	def page(self, id_):
-		global LIEN_PAGE
-		print(LIEN_PAGE % (id_))
-		(code, data) = self.__go(LIEN_PAGE % (id_))
-		return ResponseCrawler(code,data)
-	def article(self, id_):
-		global LIEN_ARTICLE
-		(code, data) = self.__go(LIEN_ARTICLE % id_.replace("_","/"))
-		return ResponseCrawler(code,data)
-	def page_random(self):
-		global LIEN_ALEATOIRE
-		(code, data) = self.__go(LIEN_ALEATOIRE)
-		return ResponseCrawler(code,data)
-	def article_random(self):
-		global LIEN_ALEATOIRE
-		(code, data) = self.__go(LIEN_ALEATOIRE)
-		if len(data) > 0:
-			data = [data[0]]
-		return ResponseCrawler(code, data)
+    """The implementation of VieDeMerde crawler"""
+    def __init__(self):
+        super().__init__(VdmParser(), HttpCommunicator())
+    def __go(self, lien):
+        print(lien)
+        reponse = self.aspirateur.get(lien)
+        if reponse["code"] == 200:
+            text = unescape(reponse["data"])
+            try:
+                data = self.parser.parse(text)
+            except ParseException as e:
+                print(e)
+                return(521,[])
+            code = 200
+        else:
+            code = reponse["code"]
+            data = []
+        return (code, data)
+    def page(self, id_):
+        global LIEN_PAGE
+        print(LIEN_PAGE % (id_))
+        (code, data) = self.__go(LIEN_PAGE % (id_))
+        return ResponseCrawler(code,data)
+    def article(self, id_):
+        global LIEN_ARTICLE
+        (code, data) = self.__go(LIEN_ARTICLE % id_.replace("__","/")+".html")
+        if len(data) > 0:
+            data = [data[0]]
+        return ResponseCrawler(code,data)
+    def page_random(self):
+        global LIEN_ALEATOIRE
+        (code, data) = self.__go(LIEN_ALEATOIRE)
+        return ResponseCrawler(code,data)
+    def article_random(self):
+        global LIEN_ALEATOIRE
+        (code, data) = self.__go(LIEN_ALEATOIRE)
+        if len(data) > 0:
+            data = [data[0]]
+        return ResponseCrawler(code, data)
