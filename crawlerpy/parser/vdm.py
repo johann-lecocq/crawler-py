a2e06836e46c95567057ff8523aad8f5bb14a810
Johann Lecocq <git-jo@johann-lecocq.fr>
2017-10-26 21:12:04 +0200

    update parser vdm for article

--- crawlerpy/parser/vdm.py
+++ crawlerpy/parser/vdm.py
@@ -13,10 +13,9 @@ class VdmParser(Parser):
     """The implementation of VideDeMerde site parser"""
     def __init__(self):
         pass
-    def __recherche_div_principal(self,racine):
+    def __recherche_div_page(self,racine):
         principal=None
         divs=racine.find_all("div")
-
         for div in divs:
             attribut=div.get("class")
             if not (attribut is None) and "infinite-scroll" in attribut:
@@ -41,22 +40,39 @@ class VdmParser(Parser):
         try:
             reponse=[]
             soup = BeautifulSoup(text, 'html.parser')
-            principal=self.__recherche_div_principal(soup)
-            articles=principal.find_all("article")
-            for article in articles:
-                attribut=article.get("class")
-                if attribut is None or "art-panel" not in attribut:
-                    continue
-                block=article.find("p")
-                content=block.find("a")
-                if 'http://www.public.fr' in content.get("href") or "http://people.premiere.fr" in content.get("href"):
-                    continue
+            if "VDM :     Aujourd" in soup.find("title").getText():
+                article = soup.find("article")
+                if article is None :
+                    return []
+                text = article.find("p")
+                if text is None:
+                    return []
+                l=article.find("a").get("href").replace("/article/","").replace("/","__").replace(".html","")
                 section=Section("text")
-                section.add_content(Data("string",content.getText()))
-                l=content.get("href").replace("/article/","").replace("/","__").replace(".html","")
+                section.add_content(Data("string",text.getText()))
                 a=Article(l)
                 a.add_section(section)
                 reponse.append(a)
+            else:
+                principal=self.__recherche_div_page(soup)
+                articles=principal.find_all("article")
+                for article in articles:
+                    attribut=article.get("class")
+                    if attribut is None or "art-panel" not in attribut:
+                        continue
+                    block=article.find("p")
+                    content=block.find("a")
+                    if 'http://www.public.fr' in content.get("href") or "http://people.premiere.fr" in content.get("href"):
+                        continue
+                    text = content.getText().strip()
+                    if text[0:11]!="Aujourd'hui" or text[-3:]!="VDM":
+                        continue
+                    section=Section("text")
+                    section.add_content(Data("string",text))
+                    l=content.get("href").replace("/article/","").replace("/","__").replace(".html","")
+                    a=Article(l)
+                    a.add_section(section)
+                    reponse.append(a)
             return reponse
         except Exception as e:
             print(e)
