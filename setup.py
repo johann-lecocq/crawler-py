f2e65a90b4ec9daac4284f2e57a9bde07c800966
Johann Lecocq <git-jo@johann-lecocq.fr>
2017-10-30 23:15:43 +0100

    update version to 1.3.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Crawler-py',
-    version='1.3.7',
+    version='1.3.8,
     url='https://github.com/johann-lecocq/crawler-py/',
     license='GNU GPL 2',
     author='Johann Lecocq',
