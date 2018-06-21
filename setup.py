816dba2e586c97e271112ab5002bce0d7dae6ef9
Johann Lecocq <git-jo@johann-lecocq.fr>
2018-06-21 20:19:38 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='Crawler-py',
+    name='crawler-py',
     version='1.3.8',
     url='http://johann-lecocq.fr/informatique-detail---bibliotheque--crawlerpy.html',
     license='GNU GPL 2',
     author='Johann Lecocq',
     author_email='git-jo@johann-lecocq.fr',
     description='A web Crawler for DTC(dans ton chat), VDM(vie de merde) and SCMB(se coucher moins bete)',
-    long_description=__doc__,
+    long_description=open('README').read(),
     packages=['crawlerpy','crawlerpy.objet','crawlerpy.parser'],
     include_package_data=True,
     zip_safe=False,
